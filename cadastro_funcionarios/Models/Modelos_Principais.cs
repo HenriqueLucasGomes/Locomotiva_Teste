072859a3b100432b2fee75bb62d30e531d233e3f
HenriqueLucasGomes <henriquelucas09@hotmail.com>
2022-04-22 22:16:09 -0300

    reestruturação dos modelos / criação do DB

--- cadastro_funcionarios/Models/Modelos_Principais.cs
+++ cadastro_funcionarios/Models/Modelos_Principais.cs
@@ -13,4 +13,13 @@ namespace api.Models
 
     }
 
+    public class Equipes
+    {
+        [Key]
+        public int Id {get; set;}
+        public string Nome {get; set;}
+        public string Setor {get; set;}
+        
+    }
+
 }
--- cadastro_funcionarios/Program.cs
+++ cadastro_funcionarios/Program.cs
@@ -8,13 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Início conexão com o banco de dados   
-string stringDeConexao1 = builder.Configuration.GetConnectionString("conexaoMySQL1");
-string stringDeConexao2 = builder.Configuration.GetConnectionString("conexaoMySQL2");
 
-builder.Services.AddDbContext<DataContext>(opt => opt.UseMySql(stringDeConexao1, ServerVersion.AutoDetect(stringDeConexao1)));
-builder.Services.AddDbContext<DataContext>(opt => opt.UseMySql(stringDeConexao2, ServerVersion.AutoDetect(stringDeConexao2)));
+// Início conexão com o banco de dados   
+string stringDeConexao = builder.Configuration.GetConnectionString("conexaoMySQL");
+builder.Services.AddDbContext<DataContext>(opt => opt.UseMySql(stringDeConexao, ServerVersion.AutoDetect(stringDeConexao)));
+// builder.Services.AddDbContext<DataContext>(opt => opt.UseMySql(stringDeConexao2, ServerVersion.AutoDetect(stringDeConexao2)));
 // Término da conexão com o banco de dados
+
 //builder.Services.AddSwaggerGen(c =>
 //{
 //    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
